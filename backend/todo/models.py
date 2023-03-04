c3dc4433898ef4f8e748d4897e4ee805c9e49763
Surya Sunkari <suryasunkari@gmail.com>
2023-03-04 01:08:55 -0600

    backend template

---
+++ backend/todo/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Todo(models.Model):
+    title=models.CharField(max_length=150)
+    description=models.CharField(max_length=500)
+    completed=models.BooleanField(default=False)
+
+    # string representation of class
+    def __str__(self):
+        return self.title
